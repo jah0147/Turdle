a30aa93e41daf7dfe7ca7db35cf2b3c28cc8e762
Jacob Howard <jah0147@auburn.edu>
2022-02-22 23:21:57 -0600

    v2 git

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ set PATH=%PATH%;C:\Program Files\Git\bin
 git init
 git remote add origin https://github.com/jah0147/turdle
 git fetch origin
-git reset --hard origin/v1
+git reset --hard origin/v2
 
 REM Finished updating the game!
 REM If you see an error that git was not recognized, please run the install.bat file and install Git
